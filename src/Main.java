5aaf2f7326f0d57b7404d03bf434bad8d9a9efa7
Сергей Коваленко <sergey_kovalenko@MacBook-Pro-Sergej.local>
2022-04-18 16:11:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+
+            double result = 0;
+            Scanner firstNum = new Scanner(System.in);
+            System.out.println("Enter first number:");
+            double num1 = firstNum.nextDouble();
+
+            Scanner operator = new Scanner(System.in);
+            System.out.println("Enter operator:");
+            String operator1 = operator.nextLine();
+
+            Scanner secondNum = new Scanner(System.in);
+            System.out.println("Enter second number:");
+            double num2 = firstNum.nextDouble();
+
+            switch (operator1) {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                case "/":
+                    if (num2 == 0.0) {
+                        break;
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+            }
+
+            if(operator1 != "/" && num2 != 0.0) {
+                System.out.println(num1 + " " + operator1 + " " + num2 + " = " + result);
+                } else {
+                System.out.println("You can't divide by 0");
+
+            }
+
+
+
+        }
+    }
+}
